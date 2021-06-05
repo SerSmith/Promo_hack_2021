41343afa3a6392c320e77633acf48489385085ae
SerSmith <46071230+SerSmith@users.noreply.github.com>
2021-06-05 22:42:50 +0300

    Update exploration.py

--- exploration.py
+++ exploration.py
@@ -18,11 +18,11 @@ WEEK_FINISH = datetime.date(2021, 3, 31).isocalendar().week
 
 LOOK_BACK = range(0, 5)
 
-def week_cycle(model, sku, week, type_, back_step):
+def week_cycle(promo_flag, sku, week, type_, back_step):
     if (week + back_step < WEEK_START) or (week + back_step > WEEK_FINISH):
         return 0
     else:
-        return model.promo[sku, week + back_step, type_]
+        return promo_flag[sku, week + back_step, type_]
 
 def func_calcualte_new_sku_uplift(sku):
     return pd.Series(1, index=sku.index)
@@ -36,8 +36,8 @@ def get_candidate_SKU(hierarchy, offers, quantity_new_SKU):
 
 
 # def calculate_promo_effect(offers, skus_to_predict):
-def calculate_added_money_week_sku_type(model, sku, week, type_):
-    return sum([type_weights[type_] * sku_weights[sku] * week_from_start_weights[t] * week_cycle(model, sku, week, type_, -t) for t in LOOK_BACK])
+def calculate_added_money_week_sku_type(promo_flag, sku, week, type_):
+    return sum([type_weights[type_] * sku_weights[sku] * week_from_start_weights[t] * week_cycle(promo_flag, sku, week, type_, -t) for t in LOOK_BACK])
 
 # def calculate_added_money_week(model, week):
 #     return sum([calculate_added_money_week_sku_type[sku, week, type_] for sku in SKUS for type_ in TYPES])
@@ -95,10 +95,10 @@ def calculate_schedule(args):
     #     return sum([model.promo[sku, w, t] for w in WEEKS for t in TYPES]) <= MAX_PROMO_FOR_PERIOD
 
 
-    model.con_max_promo_period = pyo.Constraint(SKUS, rule=con_max_promo_period)
+    # model.con_max_promo_period = pyo.Constraint(SKUS, rule=con_max_promo_period)
 
     # Целевая
-    model.OBJ = pyo.Objective(expr=sum(calculate_added_money_week_sku_type(model, s, w, t)  for w in WEEKS for s in SKUS for t in TYPES), sense=pyo.maximize)
+    model.OBJ = pyo.Objective(expr=sum(calculate_added_money_week_sku_type(model.promo, s, w, t)  for w in WEEKS for s in SKUS for t in TYPES), sense=pyo.maximize)
 
     opt = SolverFactory('cbc', executable="/usr/local/bin/cbc")
     opt.options['ratioGap'] = 0.03

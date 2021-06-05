7b9c0d02b74348f73ca377ba5604af6cbe51d297
SerSmith <46071230+SerSmith@users.noreply.github.com>
2021-06-05 17:36:33 +0300

    Update DataByDay.py

--- DataByDay.py
+++ DataByDay.py
@@ -1,12 +1,14 @@
+import pandas as pd
+import os
+
 DATA_PATH = '/home/azureuser/hack/data'
 OFFERS_FILE = '20210521_offers.csv'
 CHECKS_FILE = '20210518_checks.csv'
 
-def create_data_by_day():
-    # загрузка данных
-    offers_df = pd.read_csv(os.path.join(DATA_PATH, OFFERS_FILE))
-    checks_df = pd.read_csv(os.path.join(DATA_PATH, CHECKS_FILE))
+def create_data_by_day(checks_df, offers_df):
     
+    offers_df
+
     # оставляем только нужные фичи
     checks_df_rejected = checks_df[['day', 'sku', 'supplier_price', 'selling_price', 'num_sales']].copy()
     
@@ -28,6 +30,15 @@ def create_data_by_day():
     # сливаем аггрегированные по чекам с данными офферов
     data_by_day = checks_df_rejected_agg.join(offers_df.set_index('sku'), on='sku')
     data_by_day['day'] = pd.to_datetime(data_by_day['day'], format='%Y%m%d')
+
+    # Оставляем только строчку с ближайшей состоявшейся промоакцией
+    data_by_day['days_from_start'] = (data_by_day['day'] - data_by_day['start_date']).dt.days
+
+    future_promo_sku_df = data_by_day.groupby('sku').agg({'days_from_start': 'max'}).reset_index()
+    future_promo_sku = future_promo_sku_df.loc[future_promo_sku_df['days_from_start'] <= 0, 'sku'].unique()
+
+    data_by_day = data_by_day[(data_by_day['days_from_start'] >= 0) | (data_by_day['days_from_start'].isna()) | (data_by_day['sku'].isin(future_promo_sku)) ]
+    data_by_day = data_by_day.sort_values(by='days_from_start', ascending=False, na_position='last').groupby(['sku', 'day']).head(1)
     
     # маркируем промо (1) и предпромо (-1) периоды
     promo_condition = (
